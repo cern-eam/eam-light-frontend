babac32a90ac107a7fbe07042ebaacd23dc2e723
Lukasz Pater <Lukasz.Pater@cern.ch>
2019-12-08 00:27:52 +0100

    Adapt to new backend - add missing file

---
+++ src/ui/layout/menu/MyTeamWorkOrdersTab.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Tabs from '@material-ui/core/Tabs';
+import StyledTab from './StyledTab'
+
+export default function MyTeamWorkOrdersTab(props) {
+
+    return (
+        <Tabs style={{color: "white"}}
+              centered
+              value={props.days}
+              onChange={props.onChange}
+              indicatorColor="primary">
+            <StyledTab label="Late" value="LATE" />
+            <StyledTab label="Today" value="TODAY" />
+            <StyledTab label="Week" value="WEEK"/>
+            <StyledTab label="All" value="ALL"/>
+        </Tabs>
+    )
+}
+
