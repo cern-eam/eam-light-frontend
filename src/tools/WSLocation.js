35beebd5306b063dda185b3cff7f331e21261cc4
Miguel Ventura <miguel.ventura@cern.ch>
2020-07-28 12:14:54 +0200

    [EAM-413] Do not encode location codes (regex match endpoint)

--- src/tools/WSLocation.js
+++ src/tools/WSLocation.js
@@ -2,13 +2,13 @@ import WS from './WS';
 
 const init = (config = {}) => WS._get('/locations/init', config);
 
-const get = (code, config = {}) => WS._get(`/locations/${encodeURIComponent(code)}`, config);
+const get = (code, config = {}) => WS._get(`/locations/${code}`, config);
 
 const create = (location, config = {}) => WS._post(`/locations/`, location, config);
 
-const update = (location, config = {}) => WS._put(`/locations/${encodeURIComponent(location.code)}`, location, config)
+const update = (location, config = {}) => WS._put(`/locations/${location.code}`, location, config)
 
-const remove = (code, config = {}) => WS._delete(`/locations/${encodeURIComponent(code)}`);
+const remove = (code, config = {}) => WS._delete(`/locations/${code}`);
 
 export default {
     init,
