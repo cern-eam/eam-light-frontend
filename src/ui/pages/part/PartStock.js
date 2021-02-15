eb28435923665b579801fc6c631cb60f7661406d
Pedro Amaro <pedro.amaro@cern.ch>
2021-02-15 13:52:54 +0100

    [EAM-980] Fix equipment showing links to kiosk in part stock when not in CERN mode

--- src/ui/pages/part/PartStock.js
+++ src/ui/pages/part/PartStock.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import WSParts from '../../../tools/WSParts'
 import EISTable from 'eam-components/dist/ui/components/table';
 import {Link} from 'react-router-dom';
+import { isCernMode } from '../../components/CERNMode';
 
 function PartStock(props) {
 
@@ -16,10 +17,14 @@ function PartStock(props) {
                 let stockData = response.body.data.map(line => {
                     let linkValueAsset = '/asset/';
                     const linkValueStore = `/SSO/kiosk/${line.storeCode}/issue/employee?employee=${userID}&partCode=${partCode}`;
+
+                    const storeCodeCell = line.storeCode ?
+                        isCernMode ? <a href={linkValueStore} rel="noopener noreferrer" target="_blank">{line.storeCode}</a> : line.storeCode
+                        : ''
                     return {
                         ...line,
                         assetCode: line.assetCode ? <Link to={{pathname: linkValueAsset + line.assetCode}}>{line.assetCode}</Link> : '',
-                        storeCode: line.storeCode ? <a href={linkValueStore} rel="noopener noreferrer" target="_blank">{line.storeCode}</a> : ''
+                        storeCode: storeCodeCell
                     }}
                 );
                 setData(stockData)
