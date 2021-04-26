1b3211b1d864a4631a28310393082d5f1aa2a551
Juha Tauriainen <juha.pekka.tauriainen@cern.ch>
2021-04-26 15:45:43 +0300

    [EAM-1110] Render status and priority icons when they exist

--- src/ui/pages/work/search/WorkorderSearch.js
+++ src/ui/pages/work/search/WorkorderSearch.js
@@ -5,7 +5,38 @@ import { EAMCellField } from 'eam-components/dist/ui/components/grids/eam/utils'
 import EAMGrid from 'eam-components/dist/ui/components/grids/eam/EAMGrid';
 import SyncedQueryParamsEAMGridContext from "../../../../tools/SyncedQueryParamsEAMGridContext";
 
+
+const iconRenderer = ({ column, value }) => {
+    const [name, color] = value.split('@')
+    const baseUrl = 'https://cmmsx-test.cern.ch/web/base/resources/theme-default/images/gis/map-pin-'
+
+    if(!color || !color.length) {
+        return EAMCellField({ column, value });
+    }
+
+    const iconUrl = `${baseUrl}${color.toLowerCase().trim()}.svg`
+
+    return (
+        <>
+            <img
+                src={iconUrl}
+                alt={value}
+                style={{
+                    height: '1rem',
+                    position: 'relative',
+                    top: '2px'
+                }}
+            />
+            {name}
+        </>
+    )
+}
+
 const cellRenderer = ({ column, value }) => {
+    if (column.id === 'statusicon' || column.id === 'priorityicon') {
+        return iconRenderer({ column, value });
+    }
+
     if (column.id === 'workordernum') {
         return (
             <Typography>
@@ -13,8 +44,9 @@ const cellRenderer = ({ column, value }) => {
                     {value}
                 </Link>
             </Typography>
-        )   
+        )
     }
+
     return EAMCellField({ column, value });
 }
 
