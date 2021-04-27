f81a08a94337a4706fd348adb0336af6c1b70eb0
Juha Tauriainen <juha.pekka.tauriainen@cern.ch>
2021-04-27 13:00:40 +0300

    [EAM-1110] Change icon to a colored circle and separate the icon logic to its own component

--- src/ui/pages/work/search/WorkorderSearch.js
+++ src/ui/pages/work/search/WorkorderSearch.js
@@ -4,37 +4,11 @@ import Typography from '@material-ui/core/Typography';
 import { EAMCellField } from 'eam-components/dist/ui/components/grids/eam/utils';
 import EAMGrid from 'eam-components/dist/ui/components/grids/eam/EAMGrid';
 import SyncedQueryParamsEAMGridContext from "../../../../tools/SyncedQueryParamsEAMGridContext";
-
-
-const iconRenderer = ({ column, value }) => {
-    const [name, color] = value.split('@')
-    const baseUrl = 'https://cmmsx-test.cern.ch/web/base/resources/theme-default/images/gis/map-pin-'
-
-    if(!color || !color.length) {
-        return EAMCellField({ column, value });
-    }
-
-    const iconUrl = `${baseUrl}${color.toLowerCase().trim()}.svg`
-
-    return (
-        <>
-            <img
-                src={iconUrl}
-                alt={value}
-                style={{
-                    height: '1rem',
-                    position: 'relative',
-                    top: '2px'
-                }}
-            />
-            {name}
-        </>
-    )
-}
+import WorkorderSearchIcon from './WorkorderSearchIcon';
 
 const cellRenderer = ({ column, value }) => {
     if (column.id === 'statusicon' || column.id === 'priorityicon') {
-        return iconRenderer({ column, value });
+        return <WorkorderSearchIcon column={column} value={value} />
     }
 
     if (column.id === 'workordernum') {
