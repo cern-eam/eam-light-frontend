e493eafa5b65c77b15b822ff9f33168dc6f6b8f2
Pedro Amaro <pedro.amaro@cern.ch>
2021-05-26 10:53:54 +0200

    [EAM-1132] Filter out "MTF title" custom fields

--- src/ui/components/customfields/CustomFields.js
+++ src/ui/components/customfields/CustomFields.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import CustomFieldInput from './CustomFieldInput';
 import WSCustomFields from "../../../tools/WSCustomFields";
 import SimpleEmptyState from 'eam-components/dist/ui/components/emptystates/SimpleEmptyState'
+import { isCernMode } from "../CERNMode"
 
 function CustomFields(props) {
     let [lookupValues, setLookupValues] = useState(null);
@@ -28,7 +29,10 @@ function CustomFields(props) {
         ? <SimpleEmptyState message="No Custom Fields to show." />
         : (
             <div style={{width: "100%", marginTop: 0}}>
-                {customFields.map((customField, index) => (
+                {/* The custom fields starting with MTFX were temporarily used to have a similar functionality to
+                    the Line Title in the Associate Custom Fields screen of Infor EAM. They may now be hidden. */}
+                {customFields.filter(customField => !customField.code.startsWith('MTFX') || !isCernMode)
+                    .map((customField, index) => (
                         <CustomFieldInput children={children}
                                             key={index}
                                             updateCustomFieldValue={updateCustomFieldValue.bind(null, index)}
@@ -36,8 +40,7 @@ function CustomFields(props) {
                                             index={index}
                                             lookupValues={lookupValues}
                                             readonly={readonly}/>
-                    ))
-                }
+                ))}
             </div>
         )
     )
