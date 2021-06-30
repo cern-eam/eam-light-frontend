496e10b7b9e722d63f3f7a93ae49264dc3bc3410
Ricardo Almeida <ricardo.almeida@cern.ch>
2021-06-30 23:21:08 +0200

    Allow extra settings on ScanUserr based on url parameters

--- src/ui/components/servicelogin/ScanUser.js
+++ src/ui/components/servicelogin/ScanUser.js
@@ -5,10 +5,11 @@ import KeyCode from '../../../enums/KeyCode';
 import WS from '../../../tools/WS';
 import BlockUi from 'react-block-ui';
 import { withCernMode } from '../CERNMode';
+import GridTools from '../../../tools/GridTools';
 
-const setUser = (userId, onSuccess, onError) => {
+const setUser = (userId, onSuccess, onError, mode) => {
     if (userId) {
-        WS.getUserDataToImpersonate(userId, MODE)
+        WS.getUserDataToImpersonate(userId, mode)
             .then(resp => onSuccess(resp.body.data))
             .catch(onError)
             ;
@@ -23,9 +24,6 @@ const MODES = {
     PERSON: "PERSON",
 }
 
-const MODE = MODES.PERSON;
-const SCANNER_ONLY = true;
-
 /**
  * Allows user to scan card
  */
@@ -34,6 +32,9 @@ const ScanUser = ({ updateScannedUser, showNotification, handleError }) => {
         //const [ref, updateRef] = useState(null);
         const [loading, setLoading] = useState(null);
         const [scannerTimeout, setScannerTimeout] = useState(null);
+        const scannerOnly = GridTools.getURLParameterByName("scannerOnly") !== "false";
+        const mode = GridTools.getURLParameterByName("mode") || MODES.PERSON;
+
 
         const updateUser = (evt) => {
             if (loading) return;
@@ -51,30 +52,30 @@ const ScanUser = ({ updateScannedUser, showNotification, handleError }) => {
                 evt.target && evt.target.focus();
                 //ref && ref.focus();
             }
-            setUser(cernId, onSuccess, onError);
+            setUser(cernId, onSuccess, onError, mode);
         }
 
         return <div style={{zIndex: '1399', backgroundColor: 'rgba(0, 0, 0, 0.8)', position: 'absolute', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', display: "flex", flexDirection: 'column'}}>
             <BlockUi blocking={loading} style={{zIndex: '1399', backgroundColor: 'rgba(255, 255, 255, 1)', position: 'absolute', width: '50%', height: '50%', alignItems: 'center', justifyContent: 'center', display: "flex", flexDirection: 'column'}}>
-                <span className="FontLatoBlack Fleft Fs30 DispBlock" style={{fontSize: '18px', color: "#02a2f2"}}>{MODE === MODES.PERSON ? "Scan your CERN badge: " : "Insert your ID: "}</span>
+                <span className="FontLatoBlack Fleft Fs30 DispBlock" style={{fontSize: '18px', color: "#02a2f2"}}>{mode === MODES.PERSON ? "Scan your CERN badge: " : "Insert your ID: "}</span>
                 <Input
                     //ref={that => that.focus()}
                     autoFocus
-                    type="password"
+                    type={mode === MODES.PERSON && "password"}
                     value={cernId}
                     onChange={(event) => {
                         const value = event.target.value;
-                        if (SCANNER_ONLY && value && (value.length - (cernId || "").length) > 1) {
+                        if (scannerOnly && value && (value.length - (cernId || "").length) > 1) {
                             updateCernId("");
                         } else {
-                            updateCernId(value && (MODE === MODES.PERSON ? value.replace(/\D/g,''): value.toUpperCase()))
+                            updateCernId(value && (mode === MODES.PERSON ? value.replace(/\D/g,''): value.toUpperCase()))
                         }
                     }}
-                    placeholder={MODE === MODES.PERSON ? "Person ID" : "CERN ID, Person ID or Login"}
+                    placeholder={mode === MODES.PERSON ? "Person ID" : "CERN ID, Person ID or Login"}
                     style={{width: '200px'}}
-                    onBlur={(evt) => !SCANNER_ONLY && updateUser(evt)}
+                    onBlur={(evt) => !scannerOnly && updateUser(evt)}
                     onKeyDown={(event) => {
-                        if (SCANNER_ONLY) {
+                        if (scannerOnly) {
                             clearTimeout(scannerTimeout);
                             setScannerTimeout(setTimeout(() => updateCernId(""), 100));
                         }
