ffa7f8503df9597cc57f8bc4ace1010e6e81db10
Pedro Amaro <pedro.amaro@cern.ch>
2021-10-11 13:54:26 +0200

    [EAM-1378] My Open WOs improvements

--- src/ui/layout/menu/MyWorkOrdersTimeFilter.js
+++ src/ui/layout/menu/MyWorkOrdersTimeFilter.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Tabs from '@material-ui/core/Tabs';
 import StyledTab from './StyledTab'
+import MenuTools from './MenuTools'
 
 export default function MyWorkOrdersTimeFilter(props) {
+    const workOrders = props.workOrders;
+    
+    const makeTab = (type, label) => {
+        const count = workOrders.filter(MenuTools.daysFilterFunctions[type]).length;
+        return <StyledTab label={`${label} ${count > 99 ? '99+' : count}`} value={type} />;
+    }
 
     return (
         <Tabs style={{color: "white"}}
@@ -10,10 +17,10 @@ export default function MyWorkOrdersTimeFilter(props) {
               value={props.days}
               onChange={props.onChange}
               indicatorColor="primary">
-            <StyledTab label="Late" value="LATE" />
-            <StyledTab label="Today" value="TODAY" />
-            <StyledTab label="Week" value="WEEK"/>
-            <StyledTab label="All" value="ALL"/>
+            {makeTab('LATE', 'Late')}
+            {makeTab('TODAY', 'Today')}
+            {makeTab('WEEK', 'Week')}
+            {makeTab('ALL', 'All')}
         </Tabs>
     )
 }
