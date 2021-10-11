73b59d3c655fe9443b772fb0d4800a062beec499
Mateusz Kubicki <mateusz.jan.kubicki@cern.ch>
2021-10-11 16:38:57 +0200

    [EAM-1631] Fix no responsive bars on search view

--- src/ui/pages/search/SearchHeader.js
+++ src/ui/pages/search/SearchHeader.js
@@ -20,14 +20,36 @@ const SEARCH_TYPES = {
         code: "JOB",
     }
 }
+
+const searchIconStyle = {
+    color: "#02a2f2",
+    fontSize: 25,
+    position: "absolute",
+    right: -4,
+    top: 5
+};
+
+const PHONE_SCREEN_WIDTH = 455;
+
 export default class SearchHeader extends React.Component {
 
     state = {
         searchOn: Object.values(SEARCH_TYPES).map(v => v.value),
-    }
+        isPhoneScreen: false,
+    };
+
+    updateWidth = () => {
+        this.setState({ isPhoneScreen: this.searchBoxDiv?.clientWidth < PHONE_SCREEN_WIDTH });
+    };
 
     componentDidMount() {
         this.searchInput.focus();
+        this.updateWidth();
+        window.addEventListener('resize', this.updateWidth);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWidth);
     }
 
     renderTypeCheckbox(searchType) {
@@ -49,44 +71,74 @@ export default class SearchHeader extends React.Component {
             />
     }
 
-    render() {
-        const searchIconStyle = {
-            color: "#02a2f2",
-            fontSize: 25,
-            position: "absolute",
-            right: -4,
-            top: 5
-        };
+    renderIcon = () => (
+        <>
+            <img src="images/eamlight_logo.png" alt="EAM Light Logo" style={{paddingLeft: 20}}/>
+            <div style={{width: 10}}></div>
+            <div id="searchBoxLabelGreeting" className={this.props.searchBoxUp ? "searchBoxLabelGreetingSearch" : "searchBoxLabelGreetingHome" }>
+                <span className="FontLatoBlack Fleft Fs30 DispBlock" style={{color: "#02a2f2"}}>Welcome to EAM Light</span>
+            </div>
+        </>
+    );
 
-        const { showTypes } = this.props;
-
+    renderInput = () => {
         const entityTypes = this.state.searchOn.join(',');
-
         return (
-            <div id="searchBox" className={this.props.searchBoxUp ? "searchBox searchBoxSearch" : "searchBox searchBoxHome"}>
-                <div id="searchBoxLabel" className="searchBoxLabelHome">
-                    <img src="images/eamlight_logo.png" alt="EAM Light Logo" style={{paddingLeft: 20}}/>
-                    <div style={{width: 10}}></div>
-                    <div id="searchBoxLabelGreeting" className={this.props.searchBoxUp ? "searchBoxLabelGreetingSearch" : "searchBoxLabelGreetingHome" }>
-                        <span className="FontLatoBlack Fleft Fs30 DispBlock" style={{color: "#02a2f2"}}>Welcome to EAM Light</span>
-                    </div>
-                </div>
-                <div id="searchBoxInput" className={this.props.searchBoxUp ? "searchBoxInputSearch" : "searchBoxInputHome" }>
-                    <EAMBarcodeInput updateProperty={val => this.props.fetchDataHandler(val, entityTypes)} top={3} right={-7}>
-                        <input
-                            onInput={this.handleSearchInput.bind(this)}
-                            id="searchInputText"
-                            onKeyDown={this.props.onKeyDown}
-                            value={this.props.keyword}
-                            style={{textTransform: "uppercase"}}
-                            ref={(input) => { this.searchInput = input; }} />
-                    </EAMBarcodeInput>
-                    <FontIcon style={searchIconStyle} className="fa fa-search"/>
-                    {showTypes && <div style={{height: '30px', display: 'flex', direction: 'row', whiteSpace: 'nowrap'}}>
-                       {Object.values(SEARCH_TYPES).map(this.renderTypeCheckbox.bind(this))}
-                    </div>}
-                    <label id="searchPlaceHolder">{!this.props.keyword && "Search for Equipment, Work Orders, Parts, ..."}</label>
-                </div>
+            <EAMBarcodeInput updateProperty={val => this.props.fetchDataHandler(val, entityTypes)} top={3} right={-7}>
+                <input
+                    onInput={this.handleSearchInput.bind(this)}
+                    id="searchInputText"
+                    onKeyDown={this.props.onKeyDown}
+                    value={this.props.keyword}
+                    style={{textTransform: "uppercase"}}
+                    ref={(input) => { this.searchInput = input; }} />
+            </EAMBarcodeInput>
+        );
+    };
+
+    renderFilters = () => {
+        const { showTypes } = this.props;
+        return (
+            <>
+                <FontIcon style={searchIconStyle} className="fa fa-search"/>
+                {   
+                    showTypes &&
+                        <div className='searchTypes' style={this.state.isPhoneScreen ? { flexDirection: 'column', height: 'auto' } : {}}>
+                            {Object.values(SEARCH_TYPES).map(this.renderTypeCheckbox.bind(this))}
+                        </div>
+                }
+                <label id="searchPlaceHolder">{!this.props.keyword && "Search for Equipment, Work Orders, Parts, ..."}</label>
+            </>
+        );
+    }
+
+    render() {
+        return (
+            <div
+                id="searchBox"
+                className={this.props.searchBoxUp ? "searchBox searchBoxSearch" : "searchBox searchBoxHome"}
+                ref={searchBoxDiv => this.searchBoxDiv = searchBoxDiv}
+                style={this.props.searchBoxUp && this.state.isPhoneScreen ? { height: 'fit-content', paddingTop: '5px', paddingBottom: '5px' } : {}}
+            >
+                {
+                    this.props.searchBoxUp && this.state.isPhoneScreen ?
+                    <div id="searchBoxInput" className="searchBoxInputSearch" style={{ width: '100%'}}>
+                        {this.renderInput()}
+                        <div id="searchBoxLabel" className="searchBoxLabelHome" style={{ justifyContent: 'left'}}>
+                            {this.renderIcon()}
+                            {this.renderFilters()}
+                        </div>
+                    </div> :
+                    <>
+                        <div id="searchBoxLabel" className="searchBoxLabelHome">
+                            {this.renderIcon()}
+                        </div>
+                        <div id="searchBoxInput" className={this.props.searchBoxUp ? "searchBoxInputSearch" : "searchBoxInputHome" }>
+                            {this.renderInput()}
+                            {this.renderFilters()}
+                        </div>
+                    </>
+                }
             </div>
         );
     }
