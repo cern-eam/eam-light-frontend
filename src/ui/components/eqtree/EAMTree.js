be009195df9d13118900f2758e1b5f86396498c5
James Bradford <james.robert.bradford@cern.ch>
2021-11-15 10:22:03 +0100

    WIP: Keep-open eqp tree v1

--- src/ui/components/eqtree/EAMTree.js
+++ src/ui/components/eqtree/EAMTree.js
@@ -1,36 +1,47 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import TreeWS from './lib/TreeWS';
 import ErrorTypes from 'eam-components/dist/ui/components/eamgrid/lib/GridErrorTypes';
 import SortableTree from 'react-sortable-tree';
-import {addNodeUnderParent} from 'react-sortable-tree'
+import { addNodeUnderParent } from 'react-sortable-tree';
 import TreeTheme from './theme/TreeTheme';
 import TreeIcon from './components/TreeIcon';
-import TreeSelectParent from "./components/TreeSelectParent";
+import TreeSelectParent from './components/TreeSelectParent';
 import BlockUi from 'react-block-ui';
 
-export default function EAMTree(props) {
+const ExpandedReducer = (state, action) => {
+    switch (action.type) {
+        case 'add':
+            return state.includes(action.data) ? state : [...state, action.data];
+        case 'remove':
+            state.splice(state.indexOf(action.data), 1);
+            return state;
+        case 'reset':
+            return [];
+        default:
+            break;
+    }
+};
 
+export default function EAMTree(props) {
     const [loading, setLoading] = useState(false);
     const [treeData, setTreeData] = useState([]);
+    const [expandedState, expandedDispatch] = useReducer(ExpandedReducer, []);
 
     useEffect(() => {
         _loadTreeData(props.code);
-    }, [])
+    }, []);
 
-    useEffect( () => {
-        if (props.layout.eqpTreeNewChild && treeData.length > 0) {
-            const currentNode = props.layout.eqpTreeCurrentNode ? props.layout.eqpTreeCurrentNode.id : treeData[0].id
-            setTreeData(
-                addNodeUnderParent({
-                    treeData,
-                    parentKey: currentNode,
-                    expandParent: true,
-                    newNode: props.layout.eqpTreeNewChild,
-                    getNodeKey: ({node}) => node.id
-                }).treeData
-            )
+    useEffect(() => {
+        if (props.layout.eqpTreeParent) {
+            _loadTreeData(treeData?.[0]?.id ?? props.layout.eqpTreeParent);
         }
-    }, [props.layout.eqpTreeNewChild])
+    }, [props.layout.eqpTreeParent]);
+
+    const _reExpandNodes = (treeData) => {
+        if (treeData && treeData.length > 0) {
+            expandedState.forEach((ex) => _expandTreeToCode(treeData, ex));
+        }
+    };
 
     /**
      * Add expanded = true for those nodes that belong to the
@@ -38,13 +49,13 @@ export default function EAMTree(props) {
      */
     const _expandTreeToCode = (treeData, code) => {
         if (code && treeData && treeData.length > 0) {
-            let nodes = treeData.filter(f => (code == f.id));
+            let nodes = treeData.filter((f) => code == f.id);
 
             if (nodes.length > 0) {
                 nodes[0].expanded = true;
                 return true;
             } else {
-                return treeData.some(item => {
+                return treeData.some((item) => {
                     const res = _expandTreeToCode(item.children, code);
                     if (res) {
                         item.expanded = true;
@@ -56,26 +67,26 @@ export default function EAMTree(props) {
             }
         }
         return false;
-    }
+    };
 
     const _loadTreeData = (code) => {
         setLoading(true);
 
         TreeWS.getEquipmentStructure(code)
-            .then(data => {
-
+            .then((data) => {
                 // get tree data
                 let treeData = data.body.data;
-                _expandTreeToCode(treeData, props.code);
+                _reExpandNodes(treeData);
+                _expandTreeToCode(props.layout.eqpTreeChild);
                 setTreeData(treeData);
                 setLoading(false);
-            }).catch(error => {
-            if (error.type !== ErrorTypes.REQUEST_CANCELLED) {
-                props.handleError(error);
-            }
-        });
-
-    }
+            })
+            .catch((error) => {
+                if (error.type !== ErrorTypes.REQUEST_CANCELLED) {
+                    props.handleError(error);
+                }
+            });
+    };
 
     const _isNodeSelected = (node) => {
         if (props.layout.eqpTreeCurrentNode) {
@@ -85,11 +96,11 @@ export default function EAMTree(props) {
     };
 
     const urlTypeMap = {
-        'A': 'asset',
-        'P': 'position',
-        'S': 'system',
-        'L': 'location'
-    }
+        A: 'asset',
+        P: 'position',
+        S: 'system',
+        L: 'location',
+    };
 
     const _getColorForType = (type) => {
         switch (type) {
@@ -100,87 +111,90 @@ export default function EAMTree(props) {
             default:
                 return '#757575';
         }
-    }
+    };
 
-    const nodeClickHandler = rowInfo => event => {
-        if (event.target.className === `rowTitle` && ["A", "P", "S", "L"].includes(rowInfo.node.type)) {
-            if (window.location.pathname.includes("installeqp")) {
-                props.setLayoutProperty('eqpTreeCurrentNode', rowInfo.node)
+    const nodeClickHandler = (rowInfo) => (event) => {
+        if (event.target.className === `rowTitle` && ['A', 'P', 'S', 'L'].includes(rowInfo.node.type)) {
+            if (window.location.pathname.includes('installeqp')) {
+                props.setLayoutProperty('eqpTreeCurrentNode', rowInfo.node);
                 return;
             }
 
             if (props.history) {
-                props.history.push("/" + urlTypeMap[rowInfo.node.type] + `/${rowInfo.node.id}`);
+                props.history.push('/' + urlTypeMap[rowInfo.node.type] + `/${rowInfo.node.id}`);
             }
-            window.parent.postMessage(JSON.stringify({
-                type: 'EQUIPMENT_TREE_NODE_CLICK',
-                node: rowInfo.node,
-            }), "*");
+            window.parent.postMessage(
+                JSON.stringify({
+                    type: 'EQUIPMENT_TREE_NODE_CLICK',
+                    node: rowInfo.node,
+                }),
+                '*'
+            );
         }
-    }
+    };
 
     return (
         <React.Fragment>
-            <BlockUi tag="div" blocking={loading}/>
-            <div style={{height: '100%', margin: 5}}>
+            <BlockUi tag="div" blocking={loading} />
+            <div style={{ height: '100%', margin: 5 }}>
                 <SortableTree
                     canDrag={false}
                     treeData={treeData}
                     onChange={setTreeData}
-
                     // this is to make the tree div height to fit the screen
                     //style={{height: '100%'}}
-                    className='sortableTree'
+                    className="sortableTree"
                     // set up our theme
                     theme={TreeTheme}
-
-                    generateNodeProps={rowInfo => {
-
+                    onVisibilityToggle={({ node, expanded }) =>
+                        expandedDispatch({ type: expanded ? 'add' : 'remove', data: node.id })
+                    }
+                    generateNodeProps={(rowInfo) => {
                         return {
                             isNodeSelected: _isNodeSelected,
                             onClick: nodeClickHandler(rowInfo),
                             icons: rowInfo.node.isDirectory
                                 ? [
-                                    <div
-                                        style={{
-                                            borderLeft: 'solid 8px red',
-                                            borderBottom: 'solid 10px red',
-                                            marginRight: 10,
-                                            width: 16,
-                                            height: 12,
-                                            filter: rowInfo.node.expanded
-                                                ? 'drop-shadow(1px 0 0 red) drop-shadow(0 1px 0 red) drop-shadow(0 -1px 0 red) drop-shadow(-1px 0 0 red)'
-                                                : 'none',
-                                            borderColor: rowInfo.node.expanded ? 'white' : 'red',
-                                        }}
-                                    />,
-                                ]
+                                      <div
+                                          style={{
+                                              borderLeft: 'solid 8px red',
+                                              borderBottom: 'solid 10px red',
+                                              marginRight: 10,
+                                              width: 16,
+                                              height: 12,
+                                              filter: rowInfo.node.expanded
+                                                  ? 'drop-shadow(1px 0 0 red) drop-shadow(0 1px 0 red) drop-shadow(0 -1px 0 red) drop-shadow(-1px 0 0 red)'
+                                                  : 'none',
+                                              borderColor: rowInfo.node.expanded ? 'white' : 'red',
+                                          }}
+                                      />,
+                                  ]
                                 : [
-                                    <div className='selectParentButton' style={{marginLeft: '-10px'}}>
-                                        {
-                                            !rowInfo.parentNode && rowInfo.node.parents && rowInfo.node.parents.length > 0 &&
-                                            <TreeSelectParent
-                                                parents={rowInfo.node.parents}
-                                                reloadData={_loadTreeData}
-                                            />
-                                        }
-                                    </div>,
-                                    <TreeIcon
-                                        eqtype={rowInfo.node.type}
-                                        style={{
-                                            width: 15,
-                                            height: 15,
-                                            marginRight: 5,
-                                            marginTop: 3,
-                                            color: _getColorForType(rowInfo.node.type)
-                                        }}
-                                    />,
-                                ],
-                        }
-                    }
-                    }
+                                      <div className="selectParentButton" style={{ marginLeft: '-10px' }}>
+                                          {!rowInfo.parentNode &&
+                                              rowInfo.node.parents &&
+                                              rowInfo.node.parents.length > 0 && (
+                                                  <TreeSelectParent
+                                                      parents={rowInfo.node.parents}
+                                                      reloadData={_loadTreeData}
+                                                  />
+                                              )}
+                                      </div>,
+                                      <TreeIcon
+                                          eqtype={rowInfo.node.type}
+                                          style={{
+                                              width: 15,
+                                              height: 15,
+                                              marginRight: 5,
+                                              marginTop: 3,
+                                              color: _getColorForType(rowInfo.node.type),
+                                          }}
+                                      />,
+                                  ],
+                        };
+                    }}
                 />
             </div>
         </React.Fragment>
-    )
+    );
 }
--- src/ui/pages/equipment/installeqp/InstallEqp.js
+++ src/ui/pages/equipment/installeqp/InstallEqp.js
@@ -1,91 +1,97 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import EISPanel from 'eam-components/dist/ui/components/panel';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid'
-import WSEquipment from "../../../../tools/WSEquipment";
-import WS from "../../../../tools/WS";
-import EAMBarcodeInput from "eam-components/dist/ui/components/muiinputs/EAMBarcodeInput";
+import Grid from '@material-ui/core/Grid';
+import WSEquipment from '../../../../tools/WSEquipment';
+import WS from '../../../../tools/WS';
+import EAMBarcodeInput from 'eam-components/dist/ui/components/muiinputs/EAMBarcodeInput';
 import EAMAutocomplete from 'eam-components/dist/ui/components/muiinputs/EAMAutocomplete';
 import BlockUi from 'react-block-ui';
 
 export default function NameForm(props) {
-
-    const [parentEq, setParentEq] = useState("");
-    const [parentEqDesc, setParentEqDesc] = useState("")
-    const [childEq, setChildEq] = useState("");
+    const [parentEq, setParentEq] = useState('');
+    const [parentEqDesc, setParentEqDesc] = useState('');
+    const [childEq, setChildEq] = useState('');
     const [blocking, setBlocking] = useState(false);
 
-    useEffect( () => {
+    useEffect(() => {
         if (props.layout.eqpTreeCurrentNode) {
             setParentEq(props.layout.eqpTreeCurrentNode.id);
-            setParentEqDesc(props.layout.eqpTreeCurrentNode.name)
+            setParentEqDesc(props.layout.eqpTreeCurrentNode.name);
         }
-    },[props.layout.eqpTreeCurrentNode])
+    }, [props.layout.eqpTreeCurrentNode]);
 
     const createEquipmentStructure = (newParent, child) => {
         return {
             newParentCode: newParent,
-            childCode: child
+            childCode: child,
         };
-    }
+    };
 
     const installEqpHandler = (code) => {
         if (!parentEq || !childEq) {
-            props.showError("Please provide the Child and Parent Equipment.");
+            props.showError('Please provide the Child and Parent Equipment.');
             return;
         }
 
         setBlocking(true);
         if (code) {
-            WSEquipment.installEquipment(code).then(response => {
-                props.showNotification(`${childEq} was successfully attached to ${parentEq}`);
-                props.setLayoutProperty('eqpTreeNewChild', {id: childEq,
-                    name: response.body.data.childDesc,
-                    type: response.body.data.childType});
-                setChildEq("");
-                setParentEq("");
-                setBlocking(false);
-            }).catch(error => {
-                props.handleError(error);
-                setBlocking(false);
-            });
+            WSEquipment.installEquipment(code)
+                .then((response) => {
+                    props.showNotification(`${childEq} was successfully attached to ${parentEq}`);
+
+                    props.setLayoutProperty('eqpTreeParent', parentEq);
+                    props.setLayoutProperty('eqpTreeChild', childEq);
+                    setChildEq('');
+                    setParentEq('');
+                    setBlocking(false);
+                })
+                .catch((error) => {
+                    props.handleError(error);
+                    setBlocking(false);
+                });
         }
-    }
+    };
 
     const changeParentHandler = (code) => {
-        props.setLayoutProperty('equipment', {code: code});
+        props.setLayoutProperty('equipment', { code: code });
         props.setLayoutProperty('showEqpTreeButton', true);
-    }
+    };
 
     return (
-        <div id="entityContainer" style={{height: "100%"}}>
-            <BlockUi tag="div" blocking={blocking} style={{height: "100%", width: "100%"}}>
-                <div id="entityContent" style={{height: "100%"}}>
+        <div id="entityContainer" style={{ height: '100%' }}>
+            <BlockUi tag="div" blocking={blocking} style={{ height: '100%', width: '100%' }}>
+                <div id="entityContent" style={{ height: '100%' }}>
                     <Grid container spacing={1}>
                         <Grid item xs={12}>
                             <EISPanel heading="INSTALL EQUIPMENT">
-                                <div style={{width: "100%", marginTop: 0}}>
+                                <div style={{ width: '100%', marginTop: 0 }}>
                                     <EAMBarcodeInput updateProperty={setParentEq} right={0} top={16}>
-                                        <EAMAutocomplete elementInfo={{attribute: "R", text: "Parent"}}
-                                                         value={parentEq}
-                                                         valueKey="parent"
-                                                         onChangeValue={changeParentHandler}
-                                                         updateProperty={(key, value) => (key === 'parent') && setParentEq(value)}
-                                                         autocompleteHandler={WS.autocompleteEquipment}/>
+                                        <EAMAutocomplete
+                                            elementInfo={{ attribute: 'R', text: 'Parent' }}
+                                            value={parentEq}
+                                            valueKey="parent"
+                                            onChangeValue={changeParentHandler}
+                                            updateProperty={(key, value) => key === 'parent' && setParentEq(value)}
+                                            autocompleteHandler={WS.autocompleteEquipment}
+                                        />
                                     </EAMBarcodeInput>
 
                                     <EAMBarcodeInput updateProperty={setChildEq} right={0} top={16}>
-                                        <EAMAutocomplete elementInfo={{attribute: "R", text: "Child"}}
-                                                         value={childEq}
-                                                         valueKey="child"
-                                                         updateProperty={(key, value) => (key === 'child') && setChildEq(value)}
-                                                         autocompleteHandler={WS.autocompleteEquipment}/>
+                                        <EAMAutocomplete
+                                            elementInfo={{ attribute: 'R', text: 'Child' }}
+                                            value={childEq}
+                                            valueKey="child"
+                                            updateProperty={(key, value) => key === 'child' && setChildEq(value)}
+                                            autocompleteHandler={WS.autocompleteEquipment}
+                                        />
                                     </EAMBarcodeInput>
 
                                     <Button
-                                        style={{marginTop: 10}}
+                                        style={{ marginTop: 10 }}
                                         onClick={() => installEqpHandler(createEquipmentStructure(parentEq, childEq))}
-                                        color="primary">
+                                        color="primary"
+                                    >
                                         LINK EQUIPMENT
                                     </Button>
                                 </div>
@@ -95,5 +101,5 @@ export default function NameForm(props) {
                 </div>
             </BlockUi>
         </div>
-);
+    );
 }
