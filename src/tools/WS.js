df1ca0c399defabf2fbda96f596388684d1ee745
James Bradford <james.robert.bradford@cern.ch>
2022-03-04 17:40:38 +0100

    [EAM-1299] Added new params to eqp autocomplete

--- src/tools/WS.js
+++ src/tools/WS.js
@@ -89,7 +89,7 @@ class WS {
 
     autocompleteEquipment = (filter, config = {}) => {
         filter = encodeURIComponent(filter);
-        return this._get('/autocomplete/eqp?s=' + filter, config);
+        return this._get(`/autocomplete/eqp?s=${filter}&a=${filter}&n=${filter}`, config);
     };
 
     autocompleteEquipmentSelected = (filter, config) => {
