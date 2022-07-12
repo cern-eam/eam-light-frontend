e498f42c2b1e421f98bd1da30528552f9ce14f86
Lukasz Pater <Lukasz.Pater@cern.ch>
2022-07-12 16:27:52 +0200

    Include missing useRef import in EAMSelect

--- src/eam-components/ui/components/inputs-ng/hooks/useFetchSelectOptions.js
+++ src/eam-components/ui/components/inputs-ng/hooks/useFetchSelectOptions.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react"
+import {useState, useEffect, useRef} from "react"
 
 const useFetchSelectOptions = (autocompleteHandler, autocompleteHandlerParams = [], value, desc, options, optionsTransformer) => {
   
