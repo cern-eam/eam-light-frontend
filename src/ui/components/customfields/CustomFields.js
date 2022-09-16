ca2dcdff8130d98c9755918028f65075af3e8e64
Lukasz Pater <Lukasz.Pater@cern.ch>
2022-09-16 08:50:01 +0200

    Include keys for CF and UDF components

--- src/ui/components/customfields/CustomFields.js
+++ src/ui/components/customfields/CustomFields.js
@@ -35,6 +35,7 @@ function CustomFields(props) {
                             register={register}
                             customField={customField}
                             index={index}
+                            key={index}
                             lookupValues={lookupValues}
                         />
                     )
--- src/ui/components/userdefinedfields/UserDefinedFields.js
+++ src/ui/components/userdefinedfields/UserDefinedFields.js
@@ -10,7 +10,7 @@ const UserDefinedFields = (props) => {
         return Object.keys(entityLayout).filter(key => key.startsWith('udf'))
                                         .filter(prop => !prop.includes('Desc') && !exclusions.includes(prop))
                                         .sort( (udf1, udf2) => entityLayout[udf1].fieldGroup -  entityLayout[udf2].fieldGroup || entityLayout[udf1].fieldContainer.localeCompare(entityLayout[udf2].fieldContainer))
-                                        .map(prop => <EAMUDF {...register(prop, `userDefinedFields.${prop}`, prop.includes('Desc') ? `userDefinedFields.${prop}Desc` : undefined)}/>);
+                                        .map(prop => <EAMUDF key={prop} {...register(prop, `userDefinedFields.${prop}`, prop.includes('Desc') ? `userDefinedFields.${prop}Desc` : undefined)}/>);
     };
 
     return (
