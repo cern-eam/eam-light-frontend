db6537f3e79bdb7473fd2e6c3aa558dcdf48848d
Lukasz Pater <Lukasz.Pater@cern.ch>
2022-10-24 08:52:50 +0200

    Add padding to EAM Light Logo

--- src/ui/pages/search/SearchHeader.js
+++ src/ui/pages/search/SearchHeader.js
@@ -73,7 +73,7 @@ export default class SearchHeader extends React.Component {
 
     renderIcon = () => (
         <>
-            <img src="images/eamlight_logo.png" alt="EAM Light Logo" style={{paddingLeft: 20}}/>
+            <img src="images/eamlight_logo.png" alt="EAM Light Logo" style={{paddingLeft: 20, paddingRight: 10}}/>
             <div id="searchBoxLabelGreeting" className={this.props.searchBoxUp ? "searchBoxLabelGreetingSearch" : "searchBoxLabelGreetingHome" }>
                 <span className="FontLatoBlack Fleft Fs30 DispBlock" style={{color: "#02a2f2"}}>Welcome to EAM Light</span>
             </div>
