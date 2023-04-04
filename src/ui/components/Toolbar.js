1aaa7eb137d386b40f8ab880d9accf841a8ef6dd
Vasilis Ballas <vasileios.ballas@cern.ch>
2023-04-04 10:37:02 +0200

    [EAM-2528] Update when Repeat MTF step is visible in a workorder (#181)
    * [EAM-2528] repeat btn not visible when readonly and oos
    
    * [EAM-2528] new equipment prop and hide mtfbtn when eqpStatus is D
    
    * Revert "[EAM-2528] new equipment prop and hide mtfbtn when eqpStatus is D"
    
    This reverts commit 6c5f3c6953b14d1b1a8cec8dc656dc69cd36c35d.
    
    * Revert "[EAM-2528] repeat btn not visible when readonly and oos"
    
    This reverts commit 81907a70316b50dd4d7ea654ba7e2fa7a50b597f.
    
    * [EAM-2528] repeat btn not visible when readonly and oos and withdrawn

--- src/ui/components/Toolbar.js
+++ src/ui/components/Toolbar.js
@@ -74,7 +74,7 @@ class Toolbar extends React.Component {
     state = { watchlistOpen: false };
 
     getButtonDefinitions = () => {
-        const { copyHandler, repeatStepHandler, newEntity, entityDesc, applicationData, screencode, userGroup, entity, departmentalSecurity, screens, workorderScreencode, readOnly } = this.props;
+        const { copyHandler, repeatStepHandler, newEntity, entityDesc, applicationData, screencode, userGroup, entity, equipment, departmentalSecurity, screens, workorderScreencode, readOnly } = this.props;
 
         return {
             [BUTTON_KEYS.COPY] : {
@@ -282,7 +282,7 @@ class Toolbar extends React.Component {
                 },
             },
             [BUTTON_KEYS.REPEAT_STEP]: {
-                isVisible: () => isCernMode && entity.standardWO && entity.systemStatusCode === 'C' && entity.classCode?.startsWith('MTF'),
+                isVisible: () => isCernMode && entity.standardWO && entity.systemStatusCode === 'C' && entity.classCode?.startsWith('MTF') && !readOnly && !equipment?.outOfService && !equipment?.systemStatusCode !== 'D',
                 onClick: repeatStepHandler,
                 isDisabled: () => newEntity || departmentalSecurity?.readOnly,
                 values: {
