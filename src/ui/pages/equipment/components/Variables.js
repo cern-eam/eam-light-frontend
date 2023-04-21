3401d53e22eeee1ecbfd483a0d0a7b87c10bb1e7
Lukasz Pater <Lukasz.Pater@cern.ch>
2023-04-21 09:34:51 +0200

    Include the Variables region for A,P,S

---
+++ src/ui/pages/equipment/components/Variables.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import EAMTextField from 'eam-components/dist/ui/components/inputs-ng/EAMTextField';
+
+const Variables = (props) => {
+
+    const {register} = props;
+
+    return (
+        <React.Fragment>
+            <EAMTextField {...register('variable1', 'variable1')} />
+            <EAMTextField {...register('variable2', 'variable2')} />
+            <EAMTextField {...register('variable3', 'variable3')} />
+            <EAMTextField {...register('variable4', 'variable4')} />
+            <EAMTextField {...register('variable5', 'variable5')} />
+            <EAMTextField {...register('variable6', 'variable6')} />
+        </React.Fragment>
+    )
+}
+
+export default Variables;
