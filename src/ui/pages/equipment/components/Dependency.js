77c81c41f0ba929fc8522465d78ec24fe915c30a
IIlliirriiaann <131949142+IIlliirriiaann@users.noreply.github.com>
2024-07-17 09:49:53 +0200

    [EAM-2288] Improved dependency style

--- src/ui/pages/equipment/components/Dependency.js
+++ src/ui/pages/equipment/components/Dependency.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import LinkIcon from '@mui/icons-material/Link';
 import LinkOffIcon from '@mui/icons-material/LinkOff';
+import CircleIcon from '@mui/icons-material/Circle';
 import { Switch, Tooltip } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 
 const switchIconStyle = {
     fill: 'white',
-    borderRadius: '50%'
+    borderRadius: '50%',
+    padding: '2.5px',
+    fontSize: '1.5em',
 }
+
+const IconSwitch = styled(Switch)(({ theme }) => ({
+    opacity: '70%',
+    '& .MuiSwitch-switchBase': {
+        position: 'absolute',
+        padding: '0',
+        '&.Mui-checked': {
+            transform: 'translateX(1.25em)',
+            '& .MuiSwitch-track': {
+            backgroundColor: theme.palette.primary,
+            }
+        },
+        '&.Mui-disabled': {
+            opacity: '100%',
+            transform: 'translateX(-2.5px)',
+        }
+    },
+    '& .MuiSwitch-track': {
+        width: '3em' ,
+        height: '1em',
+        position: 'relative',
+        top: '1px',
+        transition: theme.transitions.create(['background-color'], {duration: 500}),
+        '&.Mui-disabled': {opacity: '1%'}
+    },
+    '& .MuiSwitch-thumb': {
+        position: 'relative',
+        top: '10px',
+    }
+}));
 
 const Dependency = (props) => {
 
@@ -46,18 +80,19 @@ const Dependency = (props) => {
     return (
         <>
             <Tooltip title={ 
-                isTrue(value) ? "Remove dependency on this input"
-                : "Add dependency on this input"}>
-                <Switch
-                    style = {{ height: '100%' }}
+                isTrue(value) ? "Remove dependency"
+                : "Add dependency"}>
+                <IconSwitch
+                    style = {{ height: '100%', margin: '1px 0.5em' }}
                     disabled={disabled} 
                     onChange={onChangeHandler} 
                     checked={isTrue(value)}
-                    icon={ 
-                        <LinkOffIcon 
+                    icon={
+                        disabled ? <CircleIcon style={{color: '#ddd', fontSize: '2.25em', margin: '0'}}/>
+                        : <LinkOffIcon
                         style={{ 
                             ...switchIconStyle,
-                            backgroundColor: disabled ? '#ccc' : '#737373'
+                            backgroundColor: '#737373'
                         }}/>
                     }
                     checkedIcon={
