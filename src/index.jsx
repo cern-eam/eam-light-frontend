9e831b07aaf8f7f1b25cb53e54c764f8f09dedcb
Luis Barriga Carrasco <luisbarrigacarrasco@gmail.com>
2024-10-08 12:11:36 +0200

    [EAM-3558] Manage runtime errors with overlay

--- src/index.jsx
+++ src/index.jsx
@@ -76,6 +76,17 @@ const store = createStore(
   applyMiddleware(createAxiosAuthMiddleware(), thunk)
 );
 
+// Manage runtime errors with overlay
+window.onerror = (event, source, lineno, colno, err) => {
+  // must be within function call because that's when the element is defined for sure.
+  const ErrorOverlay = customElements.get("vite-error-overlay");
+  // don't open outside vite environment
+  if (!ErrorOverlay) return;
+
+  const overlay = new ErrorOverlay(err);
+  document.body.appendChild(overlay);
+};
+
 ReactDOMClient.createRoot(document.getElementById("root")).render(
   <AuthWrapper>
     <StylesProvider jss={jss}>
