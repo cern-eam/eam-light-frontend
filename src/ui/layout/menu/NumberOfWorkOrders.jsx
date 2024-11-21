22af0551be77e286c8b262f3a02ead4db70b47e7
Lukasz Pater <Lukasz.Pater@cern.ch>
2024-11-21 11:37:09 +0100

    Move more data to Zustand - Fix Equipment Replacement

---
+++ src/ui/layout/menu/NumberOfWorkOrders.jsx
@@ -0,0 +1,28 @@
+const NumberOfWorkOrders = ({ count }) => {
+  if (!count) return null;
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        pointerEvents: "none",
+        borderRadius: "45%",
+        top: "2px",
+        right: "2px",
+        width: "11px",
+        height: "11px",
+        padding: "4px",
+        background: "#0aa2f2",
+        border: "0px solid #666",
+        color: "#fff",
+        textAlign: "center",
+        font: "10px Arial, sans-serif",
+        lineHeight: "11px",
+      }}
+    >
+      {count < 100 ? count : "99+"}
+    </div>
+  );
+};
+
+export default NumberOfWorkOrders;
