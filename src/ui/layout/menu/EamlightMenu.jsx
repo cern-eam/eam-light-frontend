1295b5518514ce1e220b4af669323e0e82708a55
Luis Barriga Carrasco <luisbarrigacarrasco@gmail.com>
2024-11-28 13:34:52 +0100

    Rename TabsSubMenus component to TabsMenuContent

--- src/ui/layout/menu/EamlightMenu.jsx
+++ src/ui/layout/menu/EamlightMenu.jsx
@@ -1,14 +1,14 @@
 import "../ApplicationLayout.css";
 import "./EamlightMenu.css";
-import TabsMenu from "./components/TabsMenu";
-import TabsSubMenus from "./components/TabsSubMenus";
+import TabsMenu from "./components/TabsMenuSidebar";
+import TabsMenuContent from "./components/TabsMenuContent";
 
 const EamlightMenu = () => {
     return (
         <div id="menu">
             <div id="menuscrollable">
                 <TabsMenu />
-                <TabsSubMenus />
+                <TabsMenuContent />
             </div>
         </div>
     );
