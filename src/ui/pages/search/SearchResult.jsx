481354a4674a181175a15f567edfd5d0810f8da2
Karol Kosek <karol99kos@gmail.com>
2025-01-15 11:26:23 +0100

    SearchResult - create Functional Component version, render it in SearchResults component

--- src/ui/pages/search/SearchResult.jsx
+++ src/ui/pages/search/SearchResult.jsx
@@ -85,3 +85,78 @@ export default class SearchResult extends React.Component {
     );
   }
 }
+
+const getSearchItemLabel = (code) => {
+  switch (code) {
+    case EntityCode.ASSET: {
+      return "Asset:";
+    }
+    case EntityCode.POSITION: {
+      return "Position:";
+    }
+    case EntityCode.SYSTEM: {
+      return "System:";
+    }
+    case EntityCode.WORKORDER: {
+      return "Work Order:";
+    }
+    case EntityCode.PART: {
+      return "Part:";
+    }
+    case EntityCode.LOCATION: {
+      return "Location: ";
+    }
+    default: {
+      return "Equipment:";
+    }
+  }
+};
+
+export function FSearchResult({ selected, data, keyword }) {
+  return (
+    <TableCell>
+      <table
+        className={
+          selected
+            ? "searchResultTableCell selectedRow"
+            : "searchResultTableCell"
+        }
+      >
+        <tbody className="searchResultRowCell">
+          <tr>
+            <td>{getSearchItemLabel(data.type)}</td>
+            <td>
+              <SearchHighlighter
+                style={{ color: "#1a0dab", fontWeight: "bold" }}
+                data={data.code}
+                keyword={keyword}
+                link={data.link}
+                type={data.type}
+              />
+            </td>
+          </tr>
+          {data.serial ? (
+            <tr>
+              <td>Serial number:</td>
+              <SearchHighlighter data={data.serial} keyword={keyword} />
+            </tr>
+          ) : null}
+          {data.alias ? (
+            <tr>
+              <td>Alias:</td>
+              <SearchHighlighter data={data.alias} keyword={keyword} />
+            </tr>
+          ) : null}
+          <tr>
+            <td>Description:</td>
+            <td>{data.description}</td>
+          </tr>
+          <tr>
+            <td>Department:</td>
+            <td>{data.mrc}</td>
+          </tr>
+        </tbody>
+      </table>
+    </TableCell>
+  );
+}
