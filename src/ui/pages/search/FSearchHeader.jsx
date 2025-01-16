3d7d0cae95832d5747fafd52509d59a8c88c5a88
Karol Kosek <karol99kos@gmail.com>
2025-01-16 22:12:06 +0100

    SearchHeader - move children components to seperate files

--- src/ui/pages/search/FSearchHeader.jsx
+++ src/ui/pages/search/FSearchHeader.jsx
@@ -1,7 +1,9 @@
 import * as React from "react";
-import FontIcon from "@mui/material/Icon";
-import EAMCheckbox from "eam-components/dist/ui/components/inputs-ng/EAMCheckbox";
+
 import EAMBarcodeScanner from "eam-components/dist/ui/components/inputs-ng/components/EAMBarcodeScanner";
+import SearchHeaderFilters from "./SearchHeaderFilters";
+import SearchHeaderIcon from "./SearchHeaderIcon";
+import SearchHeaderInput from "./SearchHeaderInput";
 
 const SEARCH_TYPES = {
   PART: {
@@ -21,114 +23,13 @@ const SEARCH_TYPES = {
   },
 };
 
-const SearchHeaderIcon = (props) => {
-  return (
-    <>
-      <img
-        src="images/eamlight_logo.png"
-        alt="EAM Light Logo"
-        style={{ paddingLeft: 20, paddingRight: 10 }}
-      />
-      <div
-        id="searchBoxLabelGreeting"
-        className={
-          props.searchBoxUp
-            ? "searchBoxLabelGreetingSearch"
-            : "searchBoxLabelGreetingHome"
-        }
-      >
-        <span
-          className="FontLatoBlack Fleft Fs30 DispBlock"
-          style={{ color: "#02a2f2" }}
-        >
-          Welcome to EAM Light
-        </span>
-      </div>
-    </>
-  );
-};
-
-const SearchHeaderInput = ({
-  searchOn = "",
-  handleSearchInput,
-  onKeyDown,
-  keyword,
-  fetchDataHandler,
-}) => {
-  const entityTypes = searchOn.join(",");
-  const searchInput = React.useRef(null);
-  React.useEffect(() => {
-    searchInput.current.focus();
-  }, []);
-
-  return (
-    <div style={{ display: "flex", alignItems: "center" }}>
-      <input
-        onChange={handleSearchInput}
-        id="searchInputText"
-        onKeyDown={onKeyDown}
-        value={keyword}
-        style={{ textTransform: "uppercase" }}
-        ref={(input) => {
-          searchInput.current = input;
-        }}
-      />
-      <EAMBarcodeScanner
-        onChange={(value) => fetchDataHandler(value, entityTypes)}
-      />
-    </div>
-  );
-};
-
-const SearchHeaderFilters = ({
-  showTypes,
-  searchOn = "",
-  setSearchOn,
-  handleSearchInput,
-  keyword,
-  isPhoneScreen,
-}) => {
-  return (
-    <>
-      {showTypes ? (
-        <div
-          className="searchTypes"
-          style={
-            isPhoneScreen ? { flexDirection: "column", height: "auto" } : {}
-          }
-        >
-          {Object.values(SEARCH_TYPES).map((searchType) => (
-            <EAMCheckbox
-              key={searchType.code}
-              label={searchType.text}
-              value={searchOn.includes(searchType.value).toString()}
-              rootStyle={{ flex: "0 1 auto" }}
-              onChange={() => {
-                setSearchOn((prevSearchOn) => {
-                  return prevSearchOn.includes(searchType.value)
-                    ? prevSearchOn.filter((val) => val !== searchType.value)
-                    : [...prevSearchOn, searchType.value];
-                });
-                handleSearchInput({
-                  target: { value: keyword },
-                });
-              }}
-            />
-          ))}
-        </div>
-      ) : null}
-      <label id="searchPlaceHolder">
-        {!keyword && "Search for Equipment, Work Orders, Parts, ..."}
-      </label>
-    </>
-  );
-};
-
-export function FSearchHeader(props) {
+function FSearchHeader(props) {
   const [searchOn, setSearchOn] = React.useState(
     Object.values(SEARCH_TYPES).map((v) => v.value)
   );
-  const [isPhoneScreen, setIsPhoneScreen] = React.useState(false);
+  // const [isPhoneScreen, setIsPhoneScreen] = React.useState(false);  - is it used?
+  const isPhoneScreen = false; // temporarly
+
   const searchBoxDiv = React.useRef(null);
 
   const handleSearchInput = (event) => {
