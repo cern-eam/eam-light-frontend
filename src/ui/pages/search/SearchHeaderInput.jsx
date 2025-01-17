e4f195cde72d5cd84f035380955436784030b930
Karol Kosek <karol99kos@gmail.com>
2025-01-17 19:49:26 +0100

    SearchHeaderInput - use debouncer to make typing expierience and results more ilustrating the state of the app

--- src/ui/pages/search/SearchHeaderInput.jsx
+++ src/ui/pages/search/SearchHeaderInput.jsx
@@ -1,42 +1,40 @@
 import React, { useEffect, useRef, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 const SearchHeaderInput = ({
   handleSearchInput,
   onKeyDown,
   value,
-  isFetching,
   children,
+  isFetching,
 }) => {
   const [searchText, setSearchText] = useState(value);
-  const [searchTextColor, setSearchTextColor] = useState("unset");
-
   const searchInput = useRef(null);
+  const propagateChangeOfValue = useDebouncedCallback(
+    () => handleSearchInput({ target: { value: searchText } }),
+    200
+  );
 
   useEffect(() => {
-    handleSearchInput({ target: { value: searchText } });
+    propagateChangeOfValue();
   }, [searchText]);
 
   useEffect(() => {
     searchInput.current.focus();
   }, []);
 
-  const previousValue = useRef(value);
-  useEffect(() => {
-    if (previousValue.current !== value && isFetching) {
-      setSearchTextColor("#737373");
-    }
-
-    if (!isFetching) setSearchTextColor("unset");
-    previousValue.current = value;
-  }, [isFetching]);
+  const searchTextColor =
+    value === searchText && !isFetching ? "unset" : "#737373";
 
   return (
     <div style={{ display: "flex", alignItems: "center" }}>
       <input
         onChange={({ target: { value } }) => setSearchText(value)}
         id="searchInputText"
-        style={{ color: searchTextColor }}
         onKeyDown={onKeyDown}
+        style={{
+          color: searchTextColor,
+        }}
         placeholder="Search for Equipment, Work Orders, Parts, ..."
         value={searchText}
         ref={(input) => {
