4d875d21a091c3226427b595d57fa7346765e960
Karol Kosek <karol99kos@gmail.com>
2025-01-17 18:59:57 +0100

    SearchHeaderInput - text is black only when it displays the results for that texts. In transition it is grey

--- src/ui/pages/search/SearchHeaderInput.jsx
+++ src/ui/pages/search/SearchHeaderInput.jsx
@@ -1,35 +1,49 @@
-import React, { useEffect, useRef } from "react";
-import EAMBarcodeScanner from "eam-components/dist/ui/components/inputs-ng/components/EAMBarcodeScanner";
+import React, { useEffect, useRef, useState } from "react";
 
 const SearchHeaderInput = ({
-  searchOn = "",
   handleSearchInput,
   onKeyDown,
   value,
-  fetchDataHandler,
+  isFetching,
+  children,
 }) => {
-  const entityTypes = searchOn.join(",");
+  const [searchText, setSearchText] = useState(value);
+  const [searchTextColor, setSearchTextColor] = useState("unset");
+
   const searchInput = useRef(null);
+
+  useEffect(() => {
+    handleSearchInput({ target: { value: searchText } });
+  }, [searchText]);
 
   useEffect(() => {
     searchInput.current.focus();
   }, []);
 
+  const previousValue = useRef(value);
+  useEffect(() => {
+    if (previousValue.current !== value && isFetching) {
+      setSearchTextColor("#737373");
+    }
+
+    if (!isFetching) setSearchTextColor("unset");
+    previousValue.current = value;
+  }, [isFetching]);
+
   return (
     <div style={{ display: "flex", alignItems: "center" }}>
       <input
-        onChange={handleSearchInput}
+        onChange={({ target: { value } }) => setSearchText(value)}
         id="searchInputText"
+        style={{ color: searchTextColor }}
         onKeyDown={onKeyDown}
         placeholder="Search for Equipment, Work Orders, Parts, ..."
-        value={value}
+        value={searchText}
         ref={(input) => {
           searchInput.current = input;
         }}
       />
-      <EAMBarcodeScanner
-        onChange={(value) => fetchDataHandler(value, entityTypes)}
-      />
+      {children}
     </div>
   );
 };
