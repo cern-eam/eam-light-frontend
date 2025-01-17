50499ebf0a7b47ca3034f3ca914df8808f1e5479
Karol Kosek <karol99kos@gmail.com>
2025-01-17 15:42:15 +0100

    Search module - cleanup

--- src/ui/pages/search/Search.jsx
+++ src/ui/pages/search/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import * as React from "react";
 import SearchResults from "./SearchResults";
 import "./Search.css";
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -10,86 +10,10 @@ import KeyCode from "eam-components/dist/enums/KeyCode";
 import ErrorTypes from "eam-components/dist/enums/ErrorTypes";
 import Ajax from "eam-components/dist/tools/ajax";
 import useSnackbarStore from "@/state/useSnackbarStore";
-
-const INITIAL_STATE = {
-  results: [],
-  searchBoxUp: false,
-  keyword: "",
-  isFetching: false,
-  redirectRoute: "",
-};
+import useSearchResources from "./useSearchResources";
 
 const prepareKeyword = (keyword) => {
   return keyword.replace("_", "\\_").replace("%", "\\%").toUpperCase();
-};
-
-const useSearchResources = (props) => {
-  const [results, setResults] = useState(INITIAL_STATE.results);
-  const [searchBoxUp, setSearchBoxUp] = useState(INITIAL_STATE.searchBoxUp);
-  const [keyword, setKeyword] = useState(INITIAL_STATE.keyword);
-  const [isFetching, setIsFetching] = useState(INITIAL_STATE.isFetching);
-  const [redirectRoute, setRedirectRoute] = useState(
-    INITIAL_STATE.redirectRoute
-  );
-  const [selectedItemIndex, setSelectedItemIndex] = useState(-1);
-
-  const timeout = useRef(null);
-  const cancelSource = useRef(null);
-  const prevProps = useRef(props);
-
-  useEffect(() => {
-    scrollWindowIfNecessary();
-    if (prevProps.current.location !== props.location) {
-      cancelSource.current && cancelSource.current.cancel();
-      setResults(INITIAL_STATE.results);
-      setSearchBoxUp(INITIAL_STATE.searchBoxUp);
-      setKeyword(INITIAL_STATE.keyword);
-      setIsFetching(INITIAL_STATE.isFetching);
-      setRedirectRoute(INITIAL_STATE.redirectRoute);
-      setSelectedItemIndex(-1);
-    }
-    prevProps.current = props;
-  }, [props.location]);
-
-  const scrollWindowIfNecessary = () => {
-    const selectedRow = document.getElementsByClassName("selectedRow")[0];
-
-    if (!selectedRow) {
-      return;
-    }
-
-    const rect = selectedRow.getBoundingClientRect();
-    const margin = 230;
-    const isInViewport =
-      rect.top >= margin &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) - margin;
-
-    if (!isInViewport) {
-      selectedRow.scrollIntoView();
-    }
-  };
-
-  return {
-    state: {
-      results,
-      setResults,
-      searchBoxUp,
-      setSearchBoxUp,
-      keyword,
-      setKeyword,
-      isFetching,
-      setIsFetching,
-      redirectRoute,
-      setRedirectRoute,
-      selectedItemIndex,
-      setSelectedItemIndex,
-    },
-    ref: {
-      timeout,
-      cancelSource,
-    },
-  };
 };
 
 function Search(props) {
