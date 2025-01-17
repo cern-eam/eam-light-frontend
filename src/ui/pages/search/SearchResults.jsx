14047993bb99bc5f587c78c208b2798eea44fa0d
Karol Kosek <karol99kos@gmail.com>
2025-01-17 10:27:43 +0100

    Remove draft FSearch components

--- src/ui/pages/search/SearchResults.jsx
+++ src/ui/pages/search/SearchResults.jsx
@@ -3,37 +3,35 @@ import SearchResult from "./SearchResult";
 import Table from "@mui/material/Table";
 import TableRow from "@mui/material/TableRow";
 import TableBody from "@mui/material/TableBody";
-import FSearchResults from "./FSearchResults";
-
-export default FSearchResults;
-class SearchResults extends React.Component {
-  render() {
-    return (
-      <div style={{ fontSize: "16px" }}>
-        <Table>
-          <TableBody>
-            {this.props.data.map(this.mapItemToSearchResult.bind(this))}
-          </TableBody>
-        </Table>
-      </div>
-    );
-  }
-
-  mapItemToSearchResult(item, number) {
-    let isSelected = item.code === this.props.selectedItemCode;
+/**
+ * Function Version of the component
+ */
+function FSearchResults(props) {
+  const mapItemToSearchResult = (item, number) => {
+    const isSelected = item.code === props.selectedItemCode;
 
     return (
       <TableRow
         key={number}
         selected={isSelected}
-        style={isSelected ? { backgroundColor: "green" } : {}}
+        style={isSelected ? { backgroundColor: "#def4fa" } : {}}
       >
         <SearchResult
           data={item}
-          keyword={this.props.keyword}
+          keyword={props.keyword}
           selected={isSelected}
         />
       </TableRow>
     );
-  }
+  };
+
+  return (
+    <div style={{ fontSize: "16px" }}>
+      <Table>
+        <TableBody>{props.data.map(mapItemToSearchResult)}</TableBody>
+      </Table>
+    </div>
+  );
 }
+
+export default FSearchResults;
