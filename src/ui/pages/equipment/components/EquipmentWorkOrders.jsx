9ef8754bf1d3d2264fc4f3cac93fa0e9be7cf88c
Karol Kosek <karol99kos@gmail.com>
2025-01-21 16:20:11 +0100

    EquipmentWorkOrders - extract elementIsClosed condition to the seperate variable

--- src/ui/pages/equipment/components/EquipmentWorkOrders.jsx
+++ src/ui/pages/equipment/components/EquipmentWorkOrders.jsx
@@ -1,160 +1,200 @@
-import React, {useState, useEffect} from 'react';
-import {format} from 'date-fns'
-import WSEquipment from '../../../../tools/WSEquipment';
-import EISTable, {TRANSFORM_KEYS} from 'eam-components/dist/ui/components/table';
-import EISTableFilter from 'eam-components/dist/ui/components/table/EISTableFilter';
-import EquipmentMTFWorkOrders from "./EquipmentMTFWorkOrders"
-import BlockUi from 'react-block-ui';
-import { isCernMode } from '../../../components/CERNMode';
-import Constants from 'eam-components/dist/enums/Constants';
-import useLocalStorage from '../../../../hooks/useLocalStorage';
+import React, { useState, useEffect } from "react";
+import { format } from "date-fns";
+import WSEquipment from "../../../../tools/WSEquipment";
+import EISTable, {
+  TRANSFORM_KEYS,
+} from "eam-components/dist/ui/components/table";
+import EISTableFilter from "eam-components/dist/ui/components/table/EISTableFilter";
+import EquipmentMTFWorkOrders from "./EquipmentMTFWorkOrders";
+import BlockUi from "react-block-ui";
+import { isCernMode } from "../../../components/CERNMode";
+import Constants from "eam-components/dist/enums/Constants";
+import useLocalStorage from "../../../../hooks/useLocalStorage";
 
 const WO_FILTER_TYPES = {
-    ALL: 'All',
-    OPEN: 'Open',
-    MTF: 'MTF',
-    THIS: 'This Eqp'
-}
+  ALL: "All",
+  OPEN: "Open",
+  MTF: "MTF",
+  THIS: "This Eqp",
+};
+// PXAFC40___-CR000077
 
 const WO_FILTERS = {
-    [WO_FILTER_TYPES.ALL]: {
-        text: WO_FILTER_TYPES.ALL,
-        process: (data) => {
-            return [...data];
-        }
+  [WO_FILTER_TYPES.ALL]: {
+    text: WO_FILTER_TYPES.ALL,
+    process: (data) => {
+      return [...data];
     },
-    [WO_FILTER_TYPES.OPEN]: {
-        text: WO_FILTER_TYPES.OPEN,
-        process: (data) => {
-            return data.filter((workOrder) => workOrder.status && ['T', 'C'].every(statusCode => !workOrder.status.startsWith(statusCode)));
-        }
+  },
+  [WO_FILTER_TYPES.OPEN]: {
+    text: WO_FILTER_TYPES.OPEN,
+    process: (data) => {
+      return data.filter(
+        (workOrder) =>
+          workOrder.status &&
+          ["T", "C"].every(
+            (statusCode) => !workOrder.status.startsWith(statusCode)
+          )
+      );
     },
-    ...(isCernMode ? {[WO_FILTER_TYPES.MTF]: {
-        text: WO_FILTER_TYPES.MTF,
-        process: (data) => {
+  },
+  ...(isCernMode
+    ? {
+        [WO_FILTER_TYPES.MTF]: {
+          text: WO_FILTER_TYPES.MTF,
+          process: (data) => {
             return data.filter((workOrder) => {
-                return workOrder.mrc && (workOrder.mrc.startsWith("ICF") || workOrder.mrc.startsWith("MTF"));
-            })
-        }
-    }} : {}),
-    [WO_FILTER_TYPES.THIS]: {
-        text: WO_FILTER_TYPES.THIS,
-        process: data => [...data]
-    }
-}
+              return (
+                workOrder.mrc &&
+                (workOrder.mrc.startsWith("ICF") ||
+                  workOrder.mrc.startsWith("MTF"))
+              );
+            });
+          },
+        },
+      }
+    : {}),
+  [WO_FILTER_TYPES.THIS]: {
+    text: WO_FILTER_TYPES.THIS,
+    process: (data) => [...data],
+  },
+};
 
-const LOCAL_STORAGE_FILTER_KEY = 'filters:workorders';
+const LOCAL_STORAGE_FILTER_KEY = "filters:workorders";
 
 function EquipmentWorkOrders(props) {
-    const { defaultFilter, equipmentcode, equipmenttype } = props;
+  const { defaultFilter, equipmentcode, equipmenttype } = props;
 
-    let [events, setEvents] = useState([]);
-    let [workorders, setWorkorders] = useState([]);
-    const [loadingData, setLoadingData] = useState(true);
+  let [events, setEvents] = useState([]);
+  let [workorders, setWorkorders] = useState([]);
+  const [loadingData, setLoadingData] = useState(true);
 
-    const [workOrderFilter, setWorkOrderFilter] = useLocalStorage(LOCAL_STORAGE_FILTER_KEY, WO_FILTER_TYPES.ALL, defaultFilter);
-    let headers = ['Work Order', 'Equipment', 'Description', 'Status', 'Date'];
-    let propCodes = ['number', 'object','desc', 'status', 'date'];
+  const [workOrderFilter, setWorkOrderFilter] = useLocalStorage(
+    LOCAL_STORAGE_FILTER_KEY,
+    WO_FILTER_TYPES.ALL,
+    defaultFilter
+  );
+  let headers = ["Work Order", "Equipment", "Description", "Status", "Date"];
+  let propCodes = ["number", "object", "desc", "status", "date"];
 
-    if (workOrderFilter === WO_FILTER_TYPES.THIS) {
-        headers = ['Work Order', 'Description', 'Status', 'Date'];
-        propCodes = ['number', 'desc', 'status', 'date'];
+  if (workOrderFilter === WO_FILTER_TYPES.THIS) {
+    headers = ["Work Order", "Description", "Status", "Date"];
+    propCodes = ["number", "desc", "status", "date"];
+  }
+
+  // make spaces in header strings non-breaking so headers do not wrap to multiple lines
+  headers = headers.map((string) => string.replaceAll(" ", "\u00a0"));
+
+  const linksMap = new Map([
+    [
+      "number",
+      {
+        linkType: "fixed",
+        linkValue: "workorder/",
+        linkPrefix: "/",
+      },
+    ],
+    [
+      "object",
+      {
+        linkType: "dynamic",
+        linkValue: "objectUrl",
+        linkPrefix: "/",
+      },
+    ],
+  ]);
+
+  const stylesMap = {
+    number: {
+      overflowWrap: "anywhere",
+    },
+  };
+
+  const keyMap = {
+    date: TRANSFORM_KEYS.DATE_DD_MMM_YYYY,
+  };
+
+  let getFilteredWorkOrderList = (workOrders) => {
+    return WO_FILTERS[workOrderFilter].process(workOrders);
+  };
+
+  useEffect(() => {
+    if (equipmentcode) {
+      fetchData(equipmentcode, equipmenttype);
+    } else {
+      setWorkorders([]);
+      setEvents([]);
     }
+  }, [equipmentcode, equipmenttype]);
 
-    // make spaces in header strings non-breaking so headers do not wrap to multiple lines
-    headers = headers.map(string => string.replaceAll(' ', '\u00a0'));
+  const fetchData = (equipmentCode, equipmentType) => {
+    Promise.all([
+      WSEquipment.getEquipmentWorkOrders(equipmentCode),
+      WSEquipment.getEquipmentEvents(equipmentCode, equipmentType),
+    ])
+      .then((responses) => {
+        const getDateLabel = (element) => {
+          const fallbackDateLabel = element.createdDate
+            ? format(new Date(element.createdDate), "dd-MMM-yyyy")
+            : "-";
 
-    const linksMap = new Map([
-        ['number', {
-            linkType: 'fixed',
-            linkValue: 'workorder/',
-            linkPrefix: '/'
-        }],
-        ['object', {
-            linkType: 'dynamic',
-            linkValue: 'objectUrl',
-            linkPrefix: '/'
-        }]
-    ]);
+          const elementIsClosed =
+            element.status.startsWith("T") || element.status.startsWith("C");
 
-    const stylesMap = {
-        number: {
-            overflowWrap: 'anywhere'
-        }
-    };
+          // closed/cancelled element
+          if (elementIsClosed)
+            return element.completedDate
+              ? format(new Date(element.completedDate), "dd-MMM-yyyy")
+              : fallbackDateLabel;
 
-    const keyMap = {
-        date: TRANSFORM_KEYS.DATE_DD_MMM_YYYY
-    }
+          // open element
+          return element.schedulingStartDate
+            ? format(new Date(element.schedulingStartDate), "dd-MMM-yyyy")
+            : fallbackDateLabel;
+        };
+        const formatResponse = (response) =>
+          response.body.data.map((element) => ({
+            ...element,
+            date: getDateLabel(element),
+            objectUrl: `equipment/${encodeURIComponent(element?.object || "")}`,
+          }));
 
-    let getFilteredWorkOrderList = (workOrders) => {
-        return WO_FILTERS[workOrderFilter].process(workOrders)
-    }
+        const [workorders, events] = responses.map(formatResponse);
+        setWorkorders(workorders);
+        setEvents(events);
+      })
+      .catch(console.error)
+      .finally(() => setLoadingData(false));
+  };
 
-    useEffect(() => {
-        if (equipmentcode) {
-            fetchData(equipmentcode, equipmenttype);
-        } else {
-            setWorkorders([]);
-            setEvents([]);
-        }
-    }, [equipmentcode, equipmenttype])
+  return (
+    <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+      <EISTableFilter
+        filters={WO_FILTERS}
+        handleFilterChange={(newFilter) => setWorkOrderFilter(newFilter)}
+        activeFilter={workOrderFilter}
+      />
 
-    const fetchData = (equipmentCode, equipmentType) => {
-        Promise.all([WSEquipment.getEquipmentWorkOrders(equipmentCode), WSEquipment.getEquipmentEvents(equipmentCode, equipmentType)])
-            .then(responses => {
-                const getDateLabel = (element) => {
-                    const fallbackDateLabel = element.createdDate ? format(new Date(element.createdDate),'dd-MMM-yyyy') : '-';
-
-                    // closed/cancelled element
-                    if(element.status.startsWith('T') || element.status.startsWith('C')) return element.completedDate ? format(new Date(element.completedDate),'dd-MMM-yyyy') : fallbackDateLabel;
-                    
-                    // open element
-                    return element.schedulingStartDate ? format(new Date(element.schedulingStartDate), 'dd-MMM-yyyy') : fallbackDateLabel
-                }
-                const formatResponse = response => response.body.data.map(element => ({
-                    ...element,
-                    date: getDateLabel(element),
-                    objectUrl: `equipment/${encodeURIComponent(element?.object || '')}`
-                }));
-
-                const [workorders, events] = responses.map(formatResponse);
-                setWorkorders(workorders);
-                setEvents(events);
-            })
-            .catch(console.error)
-            .finally(() => setLoadingData(false));
-    }
-
-    return (
-        <div style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
-            <EISTableFilter
-                filters={WO_FILTERS}
-                handleFilterChange={(newFilter) =>
-                    setWorkOrderFilter(newFilter)
-                }
-                activeFilter={workOrderFilter}
-                />
-                
-            {workOrderFilter === WO_FILTER_TYPES.MTF ?
-                <EquipmentMTFWorkOrders equipmentcode={equipmentcode} />
-                :
-                <BlockUi blocking={loadingData} style={{overflowX: 'auto'}}>
-                    <EISTable
-                    data={getFilteredWorkOrderList(workOrderFilter === WO_FILTER_TYPES.THIS ? workorders : events)}
-                    headers={headers}
-                    propCodes={propCodes}
-                    linksMap={linksMap}
-                    stylesMap={stylesMap}
-                    keyMap={keyMap}
-                    defaultOrderBy='date'
-                    defaultOrder={Constants.SORT_DESC}
-                    />
-                </BlockUi>
-            }
-        </div>
-    )
-
+      {workOrderFilter === WO_FILTER_TYPES.MTF ? (
+        <EquipmentMTFWorkOrders equipmentcode={equipmentcode} />
+      ) : (
+        <BlockUi blocking={loadingData} style={{ overflowX: "auto" }}>
+          <EISTable
+            data={getFilteredWorkOrderList(
+              workOrderFilter === WO_FILTER_TYPES.THIS ? workorders : events
+            )}
+            headers={headers}
+            propCodes={propCodes}
+            linksMap={linksMap}
+            stylesMap={stylesMap}
+            keyMap={keyMap}
+            defaultOrderBy="date"
+            defaultOrder={Constants.SORT_DESC}
+          />
+        </BlockUi>
+      )}
+    </div>
+  );
 }
 
-export default React.memo(EquipmentWorkOrders)
+export default React.memo(EquipmentWorkOrders);
