7cdfb7056685a621a5bdf7896cf7391cc6c4fcc6
Lukasz Pater <Lukasz.Pater@cern.ch>
2025-05-01 21:57:35 +0200

    Cache custom fields look up values

--- src/ui/components/customfields/tools/useCustomFieldOptions.js
+++ src/ui/components/customfields/tools/useCustomFieldOptions.js
@@ -1,13 +1,33 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+// export function useCustomFieldOptions(fetcher, code) {
+//   const [options, setOptions] = useState([]);
+
+//   useEffect(() => {
+//     fetcher(code).then((response) => {
+//       setOptions(response.body.data);
+//     });
+//   }, [fetcher, code]);
+
+//   return options;
+// }
+import { useEffect, useState } from 'react';
+import { useCustomFieldOptionsStore } from './useCustomFieldOptionsStore';
 
 export function useCustomFieldOptions(fetcher, code) {
-  const [options, setOptions] = useState([]);
+  const { cache, setCache } = useCustomFieldOptionsStore();
+  const [options, setOptions] = useState(cache[code] || []);
 
   useEffect(() => {
-    fetcher(code).then((response) => {
-      setOptions(response.body.data);
-    });
-  }, [fetcher, code]);
+    
+    if (!cache[code]) {
+      fetcher(code).then((response) => {
+        const data = response.body.data;
+        setCache(code, data);
+        setOptions(data);
+      });
+    }
+  }, [code]);
 
-  return options;
+  return cache[code] || options;
 }
