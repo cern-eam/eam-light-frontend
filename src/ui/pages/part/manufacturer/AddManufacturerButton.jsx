57e9ba6243da6b8451a5ff48db18054adf586c3b
Lukasz Pater <Lukasz.Pater@cern.ch>
2026-02-03 18:40:14 +0100

    Disable manufacturer creation in PROD

--- src/ui/pages/part/manufacturer/AddManufacturerButton.jsx
+++ src/ui/pages/part/manufacturer/AddManufacturerButton.jsx
@@ -3,12 +3,18 @@ import IconButton from "@mui/material/IconButton";
 import AddIcon from "@mui/icons-material/Add";
 import AddManufacturerDialog from "./AddManufacturerDialog";
 import useUserDataStore from "../../../../state/useUserDataStore";
+import useApplicationDataStore from "../../../../state/useApplicationDataStore";
 
 export default function AddManufacturerButton({updateEquipmentProperty, equipment}) {
   const [open, setOpen] = useState(false);
 
-  const { userData } = useUserDataStore();
-  
+  // TEMP don't render if environment is production (12.2@CERN)
+  const { applicationData } = useApplicationDataStore();
+  if (applicationData?.EL_ENVIR === "PROD") {
+    return null;
+  }
+
+  const { userData } = useUserDataStore();  
   if (!userData?.screens?.SSMANU?.creationAllowed) {
     return null;
   }
