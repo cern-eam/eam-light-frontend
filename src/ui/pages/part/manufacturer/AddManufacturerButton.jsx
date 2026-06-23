b9b78cfd623c919fa5ac96fe03911ea1e0bc3279
Lukasz Pater <Lukasz.Pater@cern.ch>
2026-06-23 16:16:35 +0200

    Always render the manufacturer button

--- src/ui/pages/part/manufacturer/AddManufacturerButton.jsx
+++ src/ui/pages/part/manufacturer/AddManufacturerButton.jsx
@@ -8,12 +8,6 @@ import useApplicationDataStore from "../../../../state/useApplicationDataStore";
 export default function AddManufacturerButton({updateEquipmentProperty, equipment}) {
   const [open, setOpen] = useState(false);
 
-  // TEMP don't render if environment is production (12.2@CERN)
-  const { applicationData } = useApplicationDataStore();
-  if (applicationData?.EL_ENVIR === "PROD") {
-    return null;
-  }
-
   const { userData } = useUserDataStore();  
   if (!userData?.screens?.SSMANU?.creationAllowed) {
     return null;
